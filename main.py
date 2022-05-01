b5dda8d446e57f572039a4b2627c40406be2445e
unknown <jachu.mail@gmail.com>
2022-05-01 22:47:58 +0200

    put cards with same numbers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
 class Card:
-
     def __init__(self, color, number):
         self.color = color
         self.number = number
@@ -13,6 +12,7 @@ class Player:
     def __init__(self, number, cards:list):
         self.number = number
         self.cards = sorted(cards, key=lambda x: x.number)
+        self.can_play = False
 
     # Doesn't work yet
     def sort_hand_by_numbers(self):
@@ -23,7 +23,7 @@ class Player:
         self.cards.sort(key=lambda x: x.color)
 
 
-    def put_cards_at_the_existing_sides(self, board):
+    def put_cards_at_ends_by_number(self, board):
         cards_to_remove_from_hand = []
         for card in self.cards:
             for board_group in board:
@@ -39,6 +39,29 @@ class Player:
         for card in cards_to_remove_from_hand:
             self.cards.remove(card)
     
+    def put_cards_at_ends_by_color(self, board):
+        cards_to_remove_from_hand = []
+        for board_group in board:
+            for card in self.cards:
+                if board_group[0].color != board_group[-1].color:
+                    if card.number == board_group[0].number and len(board_group) == 3:
+                        if card.color != board_group[0] and card.color != board_group[1] and card.color != board_group[2]:
+                            board_group.append(card)
+                            cards_to_remove_from_hand.append(card)
+        for card in cards_to_remove_from_hand:
+            self.cards.remove(card)
+
+    def check_all_possible_boards(self,board):
+        board_without_breakdowns = []
+        for set in board:
+            for element in set:
+                board_without_breakdowns.append(element)
+        for element in board_without_breakdowns:
+            print(element)
+
+
+
+
     def print_hand(self):
         print("____Start Hand____")
         for card in self.cards:
@@ -57,9 +80,11 @@ colors = [RED, BLACK, YELLOW, BLUE]
 
 test_hand_1 = [Card(RED,6), Card(BLACK,5), Card(BLUE,10), Card(YELLOW,13), Card(RED,6)]
 test_hand_2 = [Card(RED,6), Card(YELLOW,2), Card(BLUE,5), Card(YELLOW,5)]
+test_hand_3 = [Card(BLACK,3), Card(YELLOW,2), Card(BLUE,5), Card(YELLOW,5)]
+
 
 test_board_1 = [
-    [Card(RED,3), Card(RED,4), Card(RED,5)],
+    [Card(RED,3), Card(RED,3), Card(RED,5)],
     [Card(BLUE,6), Card(BLUE,7), Card(BLUE,8), Card(BLUE,9)],
     [Card(BLACK,6), Card(BLACK,7), Card(BLACK,8), Card(BLACK,9)]
 ]
@@ -71,6 +96,14 @@ test_board_2 = [
     [Card(YELLOW,1), Card(YELLOW,2), Card(YELLOW,3), Card(YELLOW,4), Card(YELLOW,5), Card(YELLOW,6), Card(YELLOW,7), Card(YELLOW,8), Card(YELLOW,9)]
 ]
 
+test_board_3 = [
+    [Card(RED,3), Card(YELLOW,3), Card(BLUE,3)],
+    [Card(BLUE,6), Card(BLUE,7), Card(BLUE,8), Card(BLUE,9)],
+    [Card(BLACK,6), Card(BLACK,7), Card(BLACK,8), Card(BLACK,9)]
+]
+
+
+
 def print_board(board):
     print("____Start Board____")
     for group in board:
@@ -78,3 +111,13 @@ def print_board(board):
         for card in group:
             print(card)
     print("____End Board____")
+
+player_1 = Player(0, test_hand_3)
+
+player_1.print_hand()
+print_board(test_board_3)
+print("|||||||||||||||||")
+player_1.put_cards_at_ends_by_color(test_board_3)
+print("|||||||||||||||||")
+player_1.print_hand()
+print_board(test_board_3)
